62db6d722476495c36a7b57f5046583f1035326d
dilshara26 <dilsharahasanka@gmail.com>
2023-05-05 12:41:10 +0530

    Initial commit

---
+++ src/com/company/DirectedG.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DirectedG {
+
+    public  HashMap<Integer, ArrayList<Integer>> adjacencyList;
+
+    public DirectedG() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(int vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(int source, int destination) {
+        if (!adjacencyList.containsKey(source)) {
+            addVertex(source);
+        }
+        if (!adjacencyList.containsKey(destination)) {
+            addVertex(destination);
+        }
+        adjacencyList.get(source).add(destination);
+    }
+
+    public ArrayList<Integer> getNeighbors(int vertex) {
+        return adjacencyList.get(vertex);
+    }
+
+    public int size() {
+        return adjacencyList.size();
+    }
+
+    public boolean hasSink() {
+        for (int vertex : adjacencyList.keySet()) {
+            if (adjacencyList.get(vertex).isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkAcyclic(){
+        boolean sink = true;
+        while (sink){
+            int v =removeSink();
+            if(v== -1){
+                sink = false;
+            }
+        }
+        return adjacencyList.size() == 0;
+    }
+
+    public int removeSink() {
+        for (int vertex : adjacencyList.keySet()) {
+            if (adjacencyList.get(vertex).isEmpty()) {
+                adjacencyList.remove(vertex);
+                for (int otherVertex : adjacencyList.keySet()) {
+                    adjacencyList.get(otherVertex).remove((Integer)vertex);
+                }
+                return vertex;
+            }
+        }
+        return -1; // indicates no sink was found
+    }
+}
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+       DirectedG d = new DirectedG();
+        System.out.println(d.size());
+
+        
+
+
+        d.addEdge(1,2);
+        d.addEdge(2,3);
+        d.addEdge(3,2);
+
+        System.out.println(d.checkAcyclic());
+
+    }
+}
